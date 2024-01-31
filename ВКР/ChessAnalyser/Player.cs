1876b432c466313373391578856f86be5cf33aa4
AzmagulovA <74853801+AzmagulovA@users.noreply.github.com>
2024-01-31 10:57:03 +0500

    Add files via upload

---
+++ ВКР/ChessAnalyser/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessAnalyser
+{
+    //класс для хранения информации о игроках и цвете победившей стороны
+    public enum Player//enum - перечисление
+    {
+        None,//значение в случае ничьи
+        White,
+        Black
+
+    }
+    public static class PlayerExtension 
+    { 
+        public static Player Opponent(this Player player)//принимает игрока и возвращает его оппонента (передача хода)
+        {
+            switch(player)
+            {
+                case Player.White:
+                    return Player.Black;
+                case Player.Black:
+                    return Player.White;
+                default:
+                    return Player.None;
+
+
+            }
+        }
+
+    }
+
+}
---
+++ ВКР/ChessAnalyser/Position.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessAnalyser
+{
+    public class Position
+    {  
+        public int Row { get; }
+        public int Column { get; }
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+        public Player SquarePlayer()//определение цвета клетки
+        {
+            if ((Row + Column) % 2 == 0) return Player.White; else return Player.Black;
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Position position &&
+                   Row == position.Row &&
+                   Column == position.Column;
+        }
+
+        public override int GetHashCode()//определяем hashCode для использования в качестве ключа в словаре
+        {
+            int hashCode = 240067226;
+            hashCode = hashCode * -1521134295 + Row.GetHashCode();
+            hashCode = hashCode * -1521134295 + Column.GetHashCode();
+            return hashCode;
+            
+        }
+
+        public static bool operator ==(Position left, Position right)
+        {
+            return EqualityComparer<Position>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Position left, Position right)
+        {
+            return !(left == right);
+        }
+        public static Position operator +(Position pos,Direction dir )
+        {
+            return new Position(pos.Row + dir.RowDelta, pos.Column + dir.ColumnDelta);
+        }
+    }
+
+}
